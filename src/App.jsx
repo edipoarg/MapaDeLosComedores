850a7c8fe072928e981ae7bac12da95f42f6ce3e
anon author <noemail@nothing.null>
2024-11-13 16:28:20 -0300

    C NAME

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Mapa from './components/mapa/Mapa';
 import Footer from './components/footer/Footer';
+import Menu from './components/navbar/Menu';
 
 function App() {
 
   return (
     <>
+    <Menu/>
       <Navbar/>
       <Mapa/>
       <Footer/>
